90024db0ee1cc12a5642c8cbed92da7c8c0230a2
Zixuan James Li <359101898@qq.com>
2022-02-25 23:05:34 -0500

    Fix app test case

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render} from '@testing-library/react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app with router', () => {
+  // render
+  render(<Router><App /></Router>);
 });
