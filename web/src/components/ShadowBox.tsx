2bc05fd157736fc8970a5d7676039bf566f784d9
Zixuan James Li <359101898@qq.com>
2022-02-26 01:45:18 -0500

    Implement ShadowBox util component

---
+++ web/src/components/ShadowBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import { WithChildren } from '../types'
+
+const ShadowBox = ({ children }: WithChildren) => {
+  return (
+    <Box component="div" sx={{ boxShadow: 8 }}>
+      {children}
+    </Box>
+  )
+}
+
+export default ShadowBox
