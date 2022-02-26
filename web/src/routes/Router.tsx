95684724b17fd03d10c322a8ee49b12a4783c88b
Zixuan James Li <359101898@qq.com>
2022-02-26 15:16:32 -0500

    Add NotFound Page

--- web/src/routes/Router.tsx
+++ web/src/routes/Router.tsx
@@ -3,14 +3,24 @@ import Home from './Home'
 import Prescriptions from './Prescriptions'
 import { Navigate, Route, Routes } from 'react-router'
 import ListAppointments from './Appointment/New/List'
+import NewAppointmentConfirm from './Appointment/New/Confirm'
+import AppointmentVideoChat from './Appointment/VideoChat'
+import NotFound from './NotFound'
 
 const PageRoutes = () => {
   return (
     <Routes>
       <Route path='/home' element={<Home />} />
       <Route path='/prescriptions' element={<Prescriptions />} />
-      <Route path='/appointment/new/list' element={<ListAppointments />} />
+      <Route path='/appointment'>
+        <Route path='new'>
+          <Route path='list' element={<ListAppointments />} />
+          <Route path='confirm' element={<NewAppointmentConfirm />} />
+        </Route>
+        <Route path='video' element={<AppointmentVideoChat />} />
+      </Route>
       <Route path='/' element={<Navigate to='/home' />} />
+      <Route path='*' element={<NotFound />}></Route>
     </Routes>
   )
 }
