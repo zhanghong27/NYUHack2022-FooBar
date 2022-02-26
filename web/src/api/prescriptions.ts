03912ea6e4b1f4f1288764704a162963e01e7575
Zixuan James Li <359101898@qq.com>
2022-02-26 17:21:54 -0500

    Deplay mock response

--- web/src/api/prescriptions.ts
+++ web/src/api/prescriptions.ts
@@ -1,21 +1,25 @@
 import { prescription } from './schemas'
 
 export const getPrescriptions = async (): Promise<prescription[]> => {
-  return [
-    {
-      name: 'Amoxicilllin 250 mg tablets',
-      instruction: '2 each day',
-      price: 23,
-    },
-    {
-      name: 'Amoxicilllin 250 mg tablets x2',
-      instruction: '2 each day',
-      price: 23,
-    },
-    {
-      name: 'Amoxicilllin 250 mg tablets x3',
-      instruction: '2 each day',
-      price: 23,
-    },
-  ]
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          name: 'Amoxicilllin 250 mg tablets',
+          instruction: '2 each day',
+          price: 23,
+        },
+        {
+          name: 'Amoxicilllin 250 mg tablets x2',
+          instruction: '2 each day',
+          price: 23,
+        },
+        {
+          name: 'Amoxicilllin 250 mg tablets x3',
+          instruction: '2 each day',
+          price: 23,
+        },
+      ])
+    }, 200)
+  })
 }
